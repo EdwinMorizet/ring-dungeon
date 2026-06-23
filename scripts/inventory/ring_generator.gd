da8810e3a828c2cea383633439030c749c931f84
edwin <edwin@arkham-studio.fr>
2026-06-23 15:25:42 +0200

    add: dummy

--- scripts/inventory/ring_generator.gd
+++ scripts/inventory/ring_generator.gd
@@ -2,8 +2,7 @@ extends RefCounted
 class_name RingGen
 
 
-
-static func generate_item(item_kind: InventoryItemDefinition.ItemKind, floor_depth: int, rng: RandomNumberGenerator) -> InventoryItemDefinition:
+static func generate_item(item_kind: InventoryItemDefinition.ItemKind, _floor_depth: int, rng: RandomNumberGenerator) -> InventoryItemDefinition:
 	var item: InventoryItemDefinition = InventoryItemDefinition.new()
 	item.item_kind = item_kind
 	item.item_id = StringName("%s_%d" % [item.get_kind_label().to_lower(), rng.randi()])
@@ -11,7 +10,7 @@ static func generate_item(item_kind: InventoryItemDefinition.ItemKind, floor_dep
 	return item
 
 static func _roll_rarity( rng: RandomNumberGenerator) -> InventoryItemDefinition.Rarity:
-	var roll: int = rng.randf()
+	var roll:= rng.randf()
 	if roll <= 0.50:
 		return InventoryItemDefinition.Rarity.COMMON
 	if roll <= 0.80:
